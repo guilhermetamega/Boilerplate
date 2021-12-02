2d4e85ba4fbfa7809ed91e94bdb5432cb45ccc99
Guilherme_Tamega <guitamega06@gmail.com>
2021-12-02 17:11:04 -0300

    Husky and staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,11 @@ module.exports = {
     browser: true,
     es2021: true,
   },
+  settings:{
+    react:{
+      "version":"detect"
+    }
+  },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
@@ -17,6 +22,12 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {},
+  plugins: ["react", "@typescript-eslint", "react-hooks"],
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off"
+  }
 };
